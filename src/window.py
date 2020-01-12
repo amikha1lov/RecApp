4c80011c565dea9bc903b8888ab4b0e7add4864b
amikha1lov <job@mikha1lov.ru>
2020-01-12 22:12:05 +0300

    add recording mode

--- src/window.py
+++ src/window.py
@@ -14,30 +14,75 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, call
 import time
+import os
 from gi.repository import Gtk
 
 
 @Gtk.Template(resource_path='/com/github/amikha1lov/recApp/window.ui')
 class RecappWindow(Gtk.ApplicationWindow):
+    video_str = "gst-launch-1.0 ximagesrc use-damage=0 show-pointer=true ! video/x-raw,framerate=25/1 ! queue ! videoscale ! videoconvert ! vp8enc min_quantizer=20 max_quantizer=20 cpu-used=2 deadline=1000000 threads=2 ! queue ! matroskamux name=mux ! queue ! filesink location='{}'.webm"
+    active_radio = "Fullscreen"
+    progname = "xwininfo"
     __gtype_name__ = 'recAppWindow'
 
     _record_button = Gtk.Template.Child()
     _stop_record_button = Gtk.Template.Child()
+    _radio_full = Gtk.Template.Child()
+    _radio_window = Gtk.Template.Child()
+    _radio_area = Gtk.Template.Child()
     _popover_about_button = Gtk.Template.Child()
+
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    @Gtk.Template.Callback()
+    def on_button_toggled(self, button):
+        if button.get_active():
+            name = button.get_label()
+            if (name == "Fullscreen"):
+                print("This is Fullscreen")
+                self.active_radio = "Fullscreen"
+                self.video_str = "gst-launch-1.0 ximagesrc use-damage=0 show-pointer=true ! video/x-raw,framerate=25/1 ! queue ! videoscale ! videoconvert ! vp8enc min_quantizer=20 max_quantizer=20 cpu-used=2 deadline=1000000 threads=2 ! queue ! matroskamux name=mux ! queue ! filesink location='{}'.webm"
+            elif (name == "Window"):
+                print("This is Window")
+                self.active_radio = "Window"
+                self.vide_str = "gst-launch-1.0 ximagesrc use-damage=0 show-pointer=true xid={} ! video/x-raw,framerate=25/1 ! queue ! videoscale ! videoconvert ! vp8enc min_quantizer=20 max_quantizer=20 cpu-used=2 deadline=1000000 threads=2 ! queue ! matroskamux name=mux ! queue ! filesink location='{}'.webm"
+            else:
+                print("This is Area")
+                self.active_radio = "Area"
+                self.vide_str = "gst-launch-1.0 ximagesrc use-damage=0 show-pointer=true startx={} starty={} endx={} endy={} ! video/x-raw,framerate=25/1 ! queue ! videoscale ! videoconvert ! vp8enc min_quantizer=20 max_quantizer=20 cpu-used=2 deadline=1000000 threads=2 ! queue ! matroskamux name=mux ! queue ! filesink location='{}'.webm"
+
+
+
+
 
     @Gtk.Template.Callback()
     def on__record_button_clicked(self, button):
+        fileName ="Recording from " + time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
+        if (self.active_radio == "Fullscreen"):
+            self.video = Popen(self.video_str.format(fileName), shell=True)
+        elif (self.active_radio == "Window"):
+            coor = Popen("xwininfo |grep 'Window id' | awk '{print $4;}'",shell=True,stdout=PIPE).communicate()
+            listCoor = list(coor)
+            listCoor = listCoor[0].decode().split()
+            xid = listCoor[0]
+            self.video = Popen(self.video_str.format(xid,fileName), shell=True)
+        else:
+            coor = Popen("slop -n -c 0.3,0.4,0.6,0.4 -l -t 0 -f '%w %h %x %y'",shell=True,stdout=PIPE).communicate()
+            listCoor = list(coor)
+            listCoor = listCoor[0].decode().split()
+            startx,starty,endx,endy=listCoor[2],listCoor[3],int(listCoor[2])+int(listCoor[0]),int(listCoor[1])+int(listCoor[3])
+            self.video = Popen(self.video_str.format(startx,starty,endx,endy,fileName), shell=True)
+
+
         self._record_button.set_visible(False)
         self._stop_record_button.set_visible(True)
         print("RECORD")
-        fileName ="Recording from " + time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
-        self.video = Popen("gst-launch-1.0 ximagesrc use-damage=0 show-pointer=true ! video/x-raw,framerate=25/1 ! queue ! videoscale ! videoconvert ! vp8enc min_quantizer=20 max_quantizer=20 cpu-used=2 deadline=1000000 threads=2 ! queue ! matroskamux name=mux ! queue ! filesink location='{}'.webm".format(fileName), shell=True)
+
+
 
 
     @Gtk.Template.Callback()
@@ -46,6 +91,12 @@ class RecappWindow(Gtk.ApplicationWindow):
         self._record_button.set_visible(True)
         print("STOP")
         self.video.terminate()
+
+
+
+
+
+
 
     @Gtk.Template.Callback()
     def on__popover_about_button_clicked(self, button):
