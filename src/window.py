acf4ec1f469e2f062d137dcf1b3b1f68e8fb96c5
amikha1lov <job@mikha1lov.ru>
2020-01-20 20:34:20 +0300

    fix: slow audio recording

--- src/window.py
+++ src/window.py
@@ -108,7 +108,7 @@ class RecappWindow(Gtk.ApplicationWindow):
                 soundOnSource = pulse.sink_list()[0].name
                 self.recordSoundOn = True
                 print(soundOnSource)
-                self.soundOn = " pulsesrc provide-clock=false device='{}.monitor' ! 'audio/x-raw,channels=2,rate=48000,format=F32LE,payload=96' ! queue ! audioconvert ! vorbisenc ! queue ! mux. -e".format(soundOnSource)
+                self.soundOn = " pulsesrc provide-clock=false device='{}.monitor' buffer-time=20000000 ! 'audio/x-raw,depth=24,channels=2,rate=44100,format=F32LE,payload=96' ! queue ! audioconvert ! vorbisenc ! queue ! mux. -e".format(soundOnSource)
                 print(self.soundOn)
         else:
             state = "off"
