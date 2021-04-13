21002ddc51291238ca5d81fc93968b9ea8668ce1
Artem Polishchuk <ego.cordatus@gmail.com>
2021-04-14 01:50:30 +0300

    build: Bump to 2.0.0-beta

--- src/recapp_constants.py
+++ src/recapp_constants.py
@@ -1,6 +1,6 @@
 recapp_constants = {
     "APPID": "com.github.amikha1lov.RecApp",
-    "APPVERSION": "1.1.0",
+    "APPVERSION": "2.0.0-beta",
     "APPNAME": "RecApp",
     "SYSNAME": "recapp",
     "RESOURCEID": "/com/github/amikha1lov/RecApp"
